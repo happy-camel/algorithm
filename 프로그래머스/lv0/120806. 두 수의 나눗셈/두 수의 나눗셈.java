0a7688005ce0e239b2b1a90525c8834e15835f42
happy-camel <120029069+happy-camel@users.noreply.github.com>
2023-02-11 23:37:52 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.02 ms, Memory: 77.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120806. 두 수의 나눗셈/두 수의 나눗셈.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        int answer = 0;
+        double A1 = num1;
+        double A2 = num2;
+        answer = (int)((A1/A2)*1000);
+        
+        return answer;
+    }
+}
