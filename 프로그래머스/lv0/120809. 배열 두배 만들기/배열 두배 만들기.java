74cf1133bf47e86e8903c6452aa283be6850f5cc
happy-camel <120029069+happy-camel@users.noreply.github.com>
2023-02-13 23:23:51 +0900

    [level 0] Title: 배열 두배 만들기, Time: 0.04 ms, Memory: 80.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120809. 배열 두배 만들기/배열 두배 만들기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] solution(int[] numbers) {
+        
+        int[] answer = numbers;
+        
+        for(int i=0; i < numbers.length; i++){
+            answer[i] = 2*numbers[i];
+        }
+        
+        
+        return answer;
+    }
+}
